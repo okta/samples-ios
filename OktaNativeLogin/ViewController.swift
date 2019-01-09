9e22c227dcbdc504ad7ef68a27e22a5db1b3c81c
Alex Lebedev <allebedew@gmail.com>
2019-01-09 11:50:24 +0200

    Initial

---
+++ OktaNativeLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OktaNativeLogin
+//
+//  Created by Alex on 9 Jan 19.
+//  Copyright © 2019 Okta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
