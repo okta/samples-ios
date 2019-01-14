35a172e948323b1b38545b90d80b2cec345c425e
Alex Lebedev <allebedew@gmail.com>
2019-01-14 14:28:45 +0200

    okta-swift added

--- OktaNativeLogin/ViewController.swift
+++ OktaNativeLogin/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import OktaAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var loginField: UITextField!
+    @IBOutlet private var passwordField: UITextField!
+    @IBOutlet private var loginButton: UIButton!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    
+    private var client: AuthenticationClient!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://lohika-um.oktapreview.com")!
+        client = AuthenticationClient(oktaDomain: url, delegate: self)
     }
-
-
+    
+    @IBAction private func loginTapped() {
+        guard let username = loginField.text,
+            let password = passwordField.text else { return }
+        
+        client.logIn(username: username, password: password)
+    }
 }
 
+extension ViewController: AuthenticationClientDelegate {
+    func handleSuccess() {
+        guard let sessionToken = client.sessionToken else { return }
+        
+        
+        
+        
+        
+    }
+    
+    func handleError(_ error: OktaError) {
+        
+    }
+    
+    func handleChangePassword(canSkip: Bool, callback: @escaping (_ old: String?, _ new: String?, _ skip: Bool) -> Void) {
+        
+    }
+    
+    func handleMultifactorAuthenication(callback: @escaping (_ code: String) -> Void) {
+        
+    }
+    
+    func transactionCancelled() {
+        
+    }
+}
