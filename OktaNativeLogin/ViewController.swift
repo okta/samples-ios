2773417c57391614ce6fdbd455d1957051a4f533
Alex Lebedev <allebedew@gmail.com>
2019-01-15 16:43:26 +0200

    UI fixes

--- OktaNativeLogin/ViewController.swift
+++ OktaNativeLogin/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import OktaAuthNative
 import OktaAuth
 
 class ViewController: UIViewController {
@@ -28,7 +29,16 @@ class ViewController: UIViewController {
         guard let username = loginField.text,
             let password = passwordField.text else { return }
         
+        loginButton.isEnabled = false
+        activityIndicator.startAnimating()
+        
         client.logIn(username: username, password: password)
+    }
+    
+    private func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
 
@@ -36,13 +46,31 @@ extension ViewController: AuthenticationClientDelegate {
     func handleSuccess() {
         guard let sessionToken = client.sessionToken else { return }
         
+        print("Session token: \(sessionToken)")
         
+        OktaAuth.authoize(withSessionToken: sessionToken).start().then { manager in
+            
+            let message = "Access token:\n\(manager.accessToken!)"
+            let alert = UIAlertController(title: "Logged In!", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.activityIndicator.stopAnimating()
         
-        
+        }.catch { error in
+            
+            print("Error: \(error)")
+            self.showError(message: error.localizedDescription)
+            self.activityIndicator.stopAnimating()
+            
+        }
         
     }
     
-    func handleError(_ error: OktaError) {
+    func handleError(_ error: OktaAuthNative.OktaError) {
+        
+        print("Error: \(error)")
+        showError(message: error.localizedDescription)
+        activityIndicator.stopAnimating()
         
     }
     
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'OktaNativeLogin' do
+	pod 'OktaAuth', :git => 'git@github.com:okta/okta-sdk-appauth-ios.git', :branch => 'session-token-to-auth-tocken-exchange'
+end
